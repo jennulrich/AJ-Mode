f55c2bb7c49f83573da176825cce34fc9bf65624
Jennifer Calipel <jennifer.calipel@gmail.com>
2018-12-05 16:20:51 +0100

    creation de page boutique

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,8 +2,20 @@
 
 namespace App\Form;
 
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
-class UserType
+class UserType extends AbstractType
 {
-
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname',TextType::class, array('label' => 'Prénom'))
+            ->add('lastname', TextType::class, array('label' => 'Nom'))
+            ->add('email', DateTimeType::class, array('label' => 'Email'))
+            ->add('is_admin', TextType::class, array('label' => 'Administrateur'))
+            ->add('password', TextType::class, array('label' => 'Mot de passe'));
+    }
 }
