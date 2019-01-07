4b1a34c62cac3c9c1a2e523e0b2ea2c42274e5b5
Jennifer Calipel <jennifer.calipel@gmail.com>
2019-01-07 17:17:45 +0100

    mise en place de la page login admin

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -88,7 +88,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         // For example : return new RedirectResponse($this->router->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
 
-        return new RedirectResponse($this->router->generate('home'));
+        return new RedirectResponse($this->router->generate('dashboard'));
     }
 
     protected function getLoginUrl()
