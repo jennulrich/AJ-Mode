5ac8c566baf60566fdb3240431e35e587a07cc1e
jennulrich <31631277+jennulrich@users.noreply.github.com>
2019-01-24 21:46:58 +0100

    Messagerie (#6)
    * test envoie de mail via le terminal & modification du controller contact
    
    * test envoie de mail via le terminal & modification du controller contact
    
    * mise en place du controller contact coté front et admin
    
    * mise en place de la page de redaction d'un email

---
+++ src/Controller/Admin/ContactController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Manager\ContactManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Contact controller.
+ *
+ * @Route("admin/mail")
+ */
+class ContactController extends Controller
+{
+    /** @var ContactManager */
+    private $contactManager;
+
+    public function __construct(ContactManager $contactManager)
+    {
+        $this->contactManager = $contactManager;
+    }
+
+    /**
+     * @Route("/{id}", name="admin_view_mail", requirements={"id"="\d+"})
+     * @param $id int
+     * @return Response
+     */
+    public function viewAction(int $id): Response
+    {
+        $contact = $this->contactManager->get($id);
+
+        return $this->render('admin/contact/detail.html.twig', [
+            "contact" => $contact
+        ]);
+    }
+
+    /**
+     * @Route("/", name="admin_mails")
+     * @param Request $request
+     * @return Response
+     */
+    public function listAction(Request $request): Response
+    {
+        $contacts = $this->contactManager->getList();
+
+        return $this->render('admin/contact/list.html.twig', [
+            "contacts" => $contacts
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/delete", name="admin_delete_mail", requirements={"id"="\d+"})
+     * @return Response
+     */
+    public function DeleteAction(Contact $contact): Response
+    {
+        $this->contactManager->save($contact);
+
+        return $this->redirectToRoute('admin_mails');
+    }
+
+    /**
+     * @Route("/compose", name="admin_compose_mail")
+     * @param Request $request
+     * @return Response
+     */
+    public function composeMailAction(Request $request): Response
+    {
+        return $this->render('admin/contact/compose.html.twig');
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname',TextType::class, array('label' => 'Prénom'))
+            ->add('lastname', TextType::class, array('label' => 'Nom'))
+            ->add('email', TextType::class, array('label' => 'Email'))
+            ->add('subject', TextType::class, array('label' => 'Sujet'))
+            ->add('message', TextareaType::class, array('label' => 'Message'));
+    }
+}
