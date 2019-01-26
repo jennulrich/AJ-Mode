5afcba8eba382201ed1f3c2df377eb6ea0f0f2e8
Jennifer Calipel <jennifer.calipel@gmail.com>
2019-01-26 16:45:21 +0100

    envoie demail depuis le back office + template emailing

--- src/Controller/Admin/ContactController.php
+++ src/Controller/Admin/ContactController.php
@@ -3,8 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use App\Entity\SendEmail;
 use App\Form\ContactType;
+use App\Form\SendEmailType;
 use App\Manager\ContactManager;
+use App\Manager\SendEmailManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,9 +24,13 @@ class ContactController extends Controller
     /** @var ContactManager */
     private $contactManager;
 
-    public function __construct(ContactManager $contactManager)
+    /** @var SendEmailManager */
+    private $sendEmailManager;
+
+    public function __construct(ContactManager $contactManager, SendEmailManager $sendEmailManager)
     {
         $this->contactManager = $contactManager;
+        $this->sendEmailManager = $sendEmailManager;
     }
 
     /**
@@ -68,9 +76,73 @@ class ContactController extends Controller
      * @Route("/compose", name="admin_compose_mail")
      * @param Request $request
      * @return Response
+     * @Method({"GET","POST"})
      */
-    public function composeMailAction(Request $request): Response
+    public function composeMailAction(\Swift_Mailer $mailer, Request $request)
     {
-        return $this->render('admin/contact/compose.html.twig');
+        $sendEmail = new SendEmail();
+        $form = $this->createForm(SendEmailType::class, $sendEmail);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($sendEmail);
+            $em->flush();
+
+
+            $message = (new \Swift_Message($sendEmail->getSubject()))
+                ->setFrom('lc.modeparis@gmail.com')
+                ->setTo($sendEmail->getDestinataire())
+                ->setBody(
+
+                    $this->renderView(
+                        'admin/emails/contact.html.twig',
+                        [
+                            'sendEmail' => $sendEmail,
+                        ]),
+                    'text/html'
+
+                );
+
+
+            $mailer->send($message);
+
+            return $this->redirectToRoute('admin_compose_mail');
+
+        }
+
+        return $this->render('admin/contact/compose.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/sent", name="admin_sent_mails")
+     * @param Request $request
+     * @return Response
+     */
+    public function sentEmailAction(Request $request): Response
+    {
+        $sentEmails = $this->sendEmailManager->getList();
+
+        return $this->render('admin/contact/sent.html.twig', [
+            "sentEmails" => $sentEmails
+        ]);
+    }
+
+    /**
+     * @Route("/sent/{id}", name="admin_view_sent_mail", requirements={"id"="\d+"})
+     * @param $id int
+     * @return Response
+     */
+    public function viewSentEmailAction(int $id): Response
+    {
+        $sentEmail = $this->sendEmailManager->get($id);
+
+        return $this->render('admin/contact/detail-sent.html.twig', [
+            "sentEmail" => $sentEmail
+        ]);
     }
 }
