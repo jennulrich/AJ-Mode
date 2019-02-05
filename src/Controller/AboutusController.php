a239341a1f881a09abef63f4c04bc52a4b897909
AmandineL <amandine.lesaint@yahoo.com>
2019-02-05 17:14:25 +0100

    envoi home et about

---
+++ src/Controller/AboutusController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amandinelesaint
+ * Date: 05/02/2019
+ * Time: 16:59
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutusController extends AbstractController
+{
+    /**
+     * @Route("/about-us", name="aboutus")
+     */
+    public function index()
+    {
+        return $this->render('front/about/aboutus.html.twig', [
+            'controller_name' => 'AboutusController',
+        ]);
+    }
+}
