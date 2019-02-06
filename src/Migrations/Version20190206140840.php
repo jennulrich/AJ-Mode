eed52aff87f3ab25169fd31d0c0e7652c4b61685
AmandineL <amandine.lesaint@yahoo.com>
2019-02-06 15:55:31 +0100

    petite modif login

--- src/Migrations/Version20190206140840.php
+++ src/Migrations/Version20190206140840.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -8,14 +10,20 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190206140437 extends AbstractMigration
+final class Version20190206140840 extends AbstractMigration
 {
+    public function getDescription() : string
+    {
+        return '';
+    }
+
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE catalog (id INT AUTO_INCREMENT NOT NULL, ref VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, price VARCHAR(255) NOT NULL, stock VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, composition VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD is_shop TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -24,5 +32,6 @@ final class Version20190206140437 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE catalog');
+        $this->addSql('ALTER TABLE user DROP is_shop');
     }
 }
