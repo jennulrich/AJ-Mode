65e45a5b87161505d4f388132bd50944483f035b
AmandineL <amandine.lesaint@yahoo.com>
2019-02-18 20:07:24 +0100

    envoi panier + validation

---
+++ src/Controller/Front/CartController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amandinelesaint
+ * Date: 18/02/2019
+ * Time: 18:33
+ */
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart", name="cart")
+     */
+    public function index()
+    {
+        return $this->render('front/cart.html.twig', [
+            'controller_name' => 'CartController',
+        ]);
+    }
+}
---
+++ src/Controller/Front/CheckoutController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amandinelesaint
+ * Date: 18/02/2019
+ * Time: 19:18
+ */
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CheckoutController extends AbstractController
+{
+    /**
+     * @Route("/checkout", name="checkout")
+     */
+    public function index()
+    {
+        return $this->render('front/checkout.html.twig', [
+            'controller_name' => 'CheckoutController',
+        ]);
+    }
+}
