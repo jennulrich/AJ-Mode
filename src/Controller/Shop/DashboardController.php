521028df145eb6142b16a44d818c286e26a07d87
Jennifer Calipel <jennifer.calipel@gmail.com>
2019-02-19 17:38:26 +0100

    modification de la liste utilisateurs partie admin

--- src/Controller/Shop/DashboardController.php
+++ src/Controller/Shop/DashboardController.php
@@ -2,22 +2,36 @@
 
 namespace App\Controller\Shop;
 
+use App\Entity\User;
+use App\Manager\UserManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
+    /** @var UserManager */
+    private $userManager;
+
+    public function __construct(UserManager $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
     /**
      * @Route("/shop/dashboard", name="shop_dashboard")
-     *
      * @IsGranted("ROLE_BOUTIQUE")
      */
     public function index()
     {
+        $users = $this->userManager->getList();
+
+
         $this->denyAccessUnlessGranted('ROLE_BOUTIQUE');
         return $this->render('shop/dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'users' => $users
         ]);
     }
 }
