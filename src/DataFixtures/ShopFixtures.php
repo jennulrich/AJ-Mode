f3fe8e204d33797440e30917536e20ec55f14524
Jennifer Calipel <jennifer.calipel@gmail.com>
2019-04-04 18:40:49 +0200

    relation boutique et catalogue

--- src/DataFixtures/ShopFixtures.php
+++ src/DataFixtures/ShopFixtures.php
@@ -13,32 +13,50 @@ class ShopFixtures extends Fixture implements DependentFixtureInterface
     {
         $shops = [
             [
-                'shop_name' => 'H&M',
-                'address' => '88 Rue de Rivoli 75004 Paris',
-                'owner' => 'test',
-                'email' => 'test@test.fr',
-                'phone' => '0 805 08 88 88'
+                'shop_name' => 'H&M'
             ],
             [
-                'shop_name' => 'Zara',
-                'address' => '45 Rue de Rennes 75006 Paris',
-                'owner' => 'test',
-                'email' => 'test@test.fr',
-                'phone' => '01 44 39 03 50'
+                'shop_name' => 'Zara'
+            ],
+            [
+                'shop_name' => 'Desigual'
+            ],
+            [
+                'shop_name' => 'Jennyfer'
+            ],
+            [
+                'shop_name' => 'Naf Naf'
+            ],
+            [
+                'shop_name' => 'Pull & Bear'
+            ],
+            [
+                'shop_name' => 'Tally Weijl'
+            ],
+            [
+                'shop_name' => 'Naf Naf'
+            ],
+            [
+                'shop_name' => 'Pull & Bear'
+            ],
+            [
+                'shop_name' => 'Tally Weijl'
             ]
         ];
 
+        $i = 0;
+
         foreach ($shops as $key => $shop) {
             $shopInfo = new Shop();
+
             $shopInfo
                 ->setShopName($shop['shop_name'])
-                ->setAddress($shop['address'])
-                ->setOwner($shop['owner'])
-                ->setEmail($shop['email'])
-                ->setPhone($shop['phone'])
                 ->setUserId($this->getReference('shop-id-'.$key));
 
             $manager->persist($shopInfo);
+
+            $this->addReference('catalog-id-'.$i, $shopInfo);
+            $i++;
         }
         $manager->flush();
     }
